4d237243254d08de2da177630448f0b083ee1fd3
motochowdhury <motochowdhury@gmail.com>
2023-03-02 23:26:25 +0600

    multiple promise consume

--- promise.js
+++ promise.js
@@ -13,6 +13,20 @@ const createdPromise = new Promise((resolve, reject) => {
   }
 });
 
+// multiple promise
+
+const userIds = [01, 02, 03, 04];
+let userData = [];
+for (i = 0; i < userIds.length; i++) {
+  const userId = userIds[i];
+  userData.push(createdPromise);
+}
+
+Promise.all(userData)
+  .then((user) => console.log(user))
+  .catch((err) => console.log(err));
+// console.log(userData);
+
 createdPromise
   .then((data) => console.log(data))
   .catch((err) => console.log(err));
