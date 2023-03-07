7a19fd9c7a1e7a3a5da664e4d651751a8bb5d120
motochowdhury <motochowdhury@gmail.com>
2023-03-07 13:00:04 +0600

    server req-res text part is done

--- handle-req-res.js
+++ handle-req-res.js
@@ -1,2 +1,20 @@
 // Core Module
 const http = require("http");
+
+// Port
+const PORT = 5000;
+
+// Create Server
+const server = http.createServer((req, res) => {
+  if (req.url == "/") {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.write("Hello this is moto from Raw server");
+    res.end();
+  }
+});
+
+// Listen
+
+server.listen(PORT, () => {
+  console.log(`Server is running at : ${PO}`);
+});
