f82c145593abb8de76951a6243e4a4b1ff24ea47
motochowdhury <motochowdhury@gmail.com>
2023-03-07 12:39:09 +0600

    Raw server has been created successfully

--- raw_server_node.js
+++ raw_server_node.js
@@ -1,1 +1,13 @@
+// Core Module
 const http = require("http");
+
+// Create raw server
+const server = http.createServer((req, res) => {
+  res.end("Hello Node.js");
+});
+
+// port
+const PORT = 5000;
+server.listen(PORT, () => {
+  console.log("server is running");
+});
