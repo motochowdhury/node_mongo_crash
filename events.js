36beebd93c9b5891ed7370dab759cbe092f7c3f4
motochowdhury <motochowdhury@gmail.com>
2023-03-13 11:02:56 +0600

    created event handler

--- events.js
+++ events.js
@@ -1,3 +1,8 @@
 // Core Module
 const events = require("events");
 const eventEmmiter = new events.EventEmitter();
+
+// Event handler
+const eventHandler = () => {
+  console.log("This is my first event handling");
+};
